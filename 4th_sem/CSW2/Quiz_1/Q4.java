32ce0cf018dfc0447ce8a6c7ad0a9dce78a214cf
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2024-04-16 20:32:18 +0530

    Q4 init

---
+++ 4th_sem/CSW2/Quiz_1/Q4.java
@@ -0,0 +1,13 @@
+package Quiz_1;
+
+public class Q4 {
+    public static void main(String[]args) {
+        System.out.println("main with string[]");
+    }
+    public static void main(String args) {
+        System.out.println("main with string");
+    }
+    public static void main() {
+        System.out.println("main without strings");
+    }
+    }
