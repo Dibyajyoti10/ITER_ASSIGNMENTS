cb9d0a1881fdba402e5fd8b125f9fb7732dc032f
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2024-04-16 20:44:02 +0530

    Create Q2.java

---
+++ 4th_sem/CSW2/Quiz_1/Q2.java
@@ -0,0 +1,22 @@
+package Quiz_1;
+
+class OverloadingCalculation2{
+	void sum(int a , long b) {
+		System.out.println("a method invoked");
+	}
+	void sum(long a, int b) {
+		System.out.println("b method invoked");
+	}
+}
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OverloadingCalculation2 oc= new OverloadingCalculation2();
+		oc.sum(20, 20);  //<--- Error occur here !
+	}
+
+}
+// output:
+// error: The method sum(int, long) is ambiguous for the type OverloadingCalculation
