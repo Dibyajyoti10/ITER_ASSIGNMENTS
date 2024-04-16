7bf6675405f62091d1ce90f6278e9df8b2d26ad9
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2024-04-16 20:39:10 +0530

    Q5 init

---
+++ 4th_sem/CSW2/Quiz_1/Q5.java
@@ -0,0 +1,19 @@
+package Quiz_1;
+
+class Adder{
+	static int add(int a, int b) {
+		return a+b;
+	}
+	static double add(int a, int b) {
+		return a+b;
+	}
+}
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(Adder.add(11, 11));
+	}
+
+}
