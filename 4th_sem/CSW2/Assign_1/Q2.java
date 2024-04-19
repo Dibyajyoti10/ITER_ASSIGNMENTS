315de0c2c5b03d987d39603873f0eee0cdd7b0c6
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2024-04-19 16:45:44 +0530

    Q2 added

---
+++ 4th_sem/CSW2/Assign_1/Q2.java
@@ -0,0 +1,47 @@
+package Assign_1;
+
+class Employee {
+    private String name;
+    private int employeeId;
+    private String department;
+    private double salary;
+    public Employee(String name, int employeeId, String department, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.department = department;
+        this.salary = salary;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public double getSalary() {
+        return salary;
+    }
+}
+public class Q2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Dibyajyoti Rautaray", 18095, "cse", 50000.0);
+        System.out.println(emp.getName());
+        System.out.println(emp.getEmployeeId());
+        System.out.println(emp.getDepartment());
+        System.out.println(emp.getSalary());
+    }
+}
