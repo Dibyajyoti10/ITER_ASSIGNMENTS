b1c9d5561d21f72f1456a473a1fe2ad2a55edc99
Dibyajyoti10 <156401898+Dibyajyoti10@users.noreply.github.com>
2024-04-19 16:48:32 +0530

    Q3 added

---
+++ 4th_sem/CSW2/Assign_1/Q3.java
@@ -0,0 +1,26 @@
+package Assign_1;
+
+class MobileApp {
+    private String sim;
+    public MobileApp(String sim) {
+        this.sim = sim;
+    }
+    public void setSim(String sim) {
+        this.sim = sim;
+    }
+    public String getSim() {
+        return sim;
+    }
+}
+public class Q3 {
+    public static void main(String[] args) {
+        MobileApp mobile1 = new MobileApp("Vodafone");
+        System.out.println("SIM used in mobile2: " + mobile1.getSim());
+        mobile1.setSim("Airtel");
+        System.out.println("SIM used in mobile1 after change: " + mobile1.getSim());
+    }
+}
+
+//output:
+//SIM used in mobile2: Vodafone
+//SIM used in mobile1 after change: Airtel
